936bf8763182e955f80566aa1023152916eee9b7
Ryan Sandor Richards <sandor.richards@gmail.com>
2015-03-26 01:58:52 -0700

    Minor fixes.

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 'use strict';
 
-var traversal = require('./lib/traversal.js');
-
-/* jshint ignore:start */
-if (typeof window !== 'undefined') {
-  window.traversal = traversal;
-}
-else if (typeof module !== 'undefined') {
-  module.exports = traversal;
-}
-/* jshint ignore:end */
+module.exports = require('./lib/traversal.js');
